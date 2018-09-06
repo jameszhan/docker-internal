c0fb3a1ac3d3372635b877622a833ac9052eebb4
James Zhan <zhiqiangzhan@gmail.com>
2018-09-06 15:49:55 +0800

    add linux sys call

--- chroot/chroot_rootfs.py
+++ chroot/chroot_rootfs.py
@@ -4,31 +4,27 @@
 import os
 import sys
 
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: chroot [COMMAND...] \n")
     else:
         root = sys.argv[1]
-        print("newroot = {}".format(root))
+        print("root = {}".format(root))
 
-        if os.chroot(root):
-            print("chroot {} error.".format(root))
-            sys.exit(1)
+        try:
+            os.chroot(root)
+            os.chdir("/")
 
-        if os.chdir("/"):
-            print("chdir / error.")
-            sys.exit(1)
+            if len(sys.argv) == 2:
+                argv = []
+                shell = os.getenv("SHELL")
+                if not shell:
+                    shell = "/bin/bash"
+                argv.append(shell)
+                argv.append("-i")
+            else:
+                argv = sys.argv[2:]
 
-        if len(sys.argv) == 2:
-            argv = []
-            shell = os.getenv("SHELL")
-            if not shell:
-                shell = "/bin/bash"
-            argv.append(shell)
-            argv.append("-i")
-        else:
-            argv = sys.argv[2:]
-
-        os.execvp(argv[0], argv)
-        print("chroot: cannot run command `{}`".format(argv))
+            os.execvp(argv[0], argv)
+        except OSError as e:
+            print("OSError: {0} with command {1}".format(e, sys.argv))
